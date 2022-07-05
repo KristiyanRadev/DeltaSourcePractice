ef1fc54bd9410b6ece92de507646b9f635b9d7c9
KristiyanRadev <kristiyanradev19b@gmail.com>
2022-07-05 12:55:53 +0300

    Initial commit commit message

--- Test.java
+++ Test.java
@@ -1,2 +1,19 @@
-public class Test {
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class USD_To_BGN {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        Scanner scanner = new Scanner(System.in);
+
+        double USD = Double.parseDouble(scanner.nextLine());
+        double BGN = USD * 1.79549 ;
+
+        System.out.println(df.format(BGN) + " BGN");
+    }
 }
+
+
+
+
+
